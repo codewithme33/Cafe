8e58e2ab357462e98fbf1c1534e3c0d80073f399
dhamisushil333 <dhamisushil333@gmail.com>
2025-01-07 12:31:24 +0545

    add images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
-    <h1>Hi</h1>
+      <NavBar/>
     </>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import backgroundImage from "../images/frame.png";
+const NavBar = () => {
+  const navBarStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    minHeight: "100vh",
+    width: "100%",
+  };
+ 
+  return (
+    <Navbar expand="lg" style={navBarStyle} className="text-light shadow-lg">
+      <Container>
+        <Navbar.Brand href="#home">My Cafe</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarResponsive" />
+        <Navbar.Collapse id="navbarResponsive">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className="active">
+              Home
+            </Nav.Link>
+            <NavDropdown title="Menu" id="navbarDropdown" menuVariant="dark">
+              <NavDropdown.Item href="#snacks">Snacks</NavDropdown.Item>
+              <NavDropdown.Item href="#main-courses">
+                Main Courses
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#desserts">Desserts</NavDropdown.Item>
+              <NavDropdown.Item href="#beverages">Beverages</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#about-us">About Us</Nav.Link>
+            <Nav.Link href="#contacts">Contacts</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
