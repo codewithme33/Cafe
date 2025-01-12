16dd9a94d0202e216c1934c1e8d9a88f9ad73054
dhamisushil333 <dhamisushil333@gmail.com>
2025-01-13 00:02:53 +0545

    updated banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,27 +1,33 @@
 import './Banner.css';
-import clockIcon from '../images/ic_watch_later.png'
-import locationIcon from '../images/ic_near_me.png'
-import phoneIcon from '../images/Gray 85.png'
+import clockIcon from '../images/ic_watch_later.png';
+import locationIcon from '../images/ic_near_me.png';
+import phoneIcon from '../images/ic_call.png';
 
-export default function Banner (){
+export default function Banner() {
+    const bannerItems = [
+        { img: clockIcon, text: (
+            <>
+        <p id='time'>10:00 AM to 7:00 PM</p>
+        <p id='workinghour' >WORKING HOURS</p>
+        </> 
+        ),},
+        { img: locationIcon, text: 'Find us here!' },
+        { img: phoneIcon, text: 'Contact us now!' }
+    ];
 
     return (
-        <div className='Banner'>
+        <div className="banner">
             <h1>Welcome to My Cafe – where flavor and comfort meet!</h1>
-            <p>"My Cafe offers great coffee, delicious food, and a cozy ambiance perfect for any moment!" </p>
+            <p>
+                My Cafe offers great coffee, delicious food, and a cozy ambiance perfect for any moment!
+            </p>
             <ul>
-                <li>
-                    <img src={clockIcon}/>
-                    <p>Open 24/7</p>
-                </li>
-                <li>
-                <img src={locationIcon}/>
-                <p>Open 24/7</p>
-                </li>
-                <li>
-                <img src={phoneIcon}/>
-                <p>Open 24/7</p>
-                </li>
+                {bannerItems.map((item, index) => (
+                    <li key={index}>
+                        <img src={item.img} alt={`icon${index + 1}`} />
+                        <p>{item.text}</p>
+                    </li>
+                ))}
             </ul>
         </div>
     );
